280e55efc2b17b786dd3aefa1130fe3bcef7afc9
Michelle Johnson <mchlljy@mit.edu>
2013-01-14 01:40:04 -0500

    deleting soundscape

--- soundscape/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Soundscape::Application.config.secret_token = '46db2080c17e144652c0ceda146d28e5caa994eb55925a3f93691b1496b8da783d4c23609bb3b1872fd9f93107f28f42e3ef17f59d5e95c92153461b254e2a39'
