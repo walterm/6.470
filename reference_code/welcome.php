a490b5a292fc07cfbd7efa320e20d14df147b311
Walter Menendez <walterm@mit.edu>
2013-01-19 00:14:27 -0500

    Milestone 2 work
    Started milestone 2, added some code that will be helpful for modeling
    the login page and the dynamic page generation.

---
+++ reference_code/welcome.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>This is welcome.php</title>
+	</head>
+	<body>
+		The name that was submitted was: &nbsp;
+		<?php echo $_POST["name"]; ?> <br />
+		The phone number that was submitted was: &nbsp;
+		<?php echo $_POST['phone']; ?> <br />
+	</body>
+</hmtl>
